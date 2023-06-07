0b8d4f1d4acacd78e6815530f869d152520f1a39
17thi_v <17thi_v@bexleygs.co.uk>
2023-06-07 10:16:08 +0100

    sorting

--- src/com/company/BookingList.java
+++ src/com/company/BookingList.java
@@ -9,7 +9,13 @@ public class BookingList {
 	public BookingList() {
 		bookings = new ArrayList<Booking>();
 	}
-	
+
+	//construct with existing datafile (probably better)
+	public BookingList(String filename){
+		bookings = new ArrayList<Booking>();
+		getBookingsFromData(filename,bookings);
+	}
+
 	private void addBooking(Booking booking){
 		bookings.add(booking);
 	}
@@ -45,6 +51,8 @@ public class BookingList {
 	//TODO make functions that sort the bookings
 	//Sort by date
 	//first sort by month then sort again by day, change the inputted array to the sorted one
+
+
 	public void sortByDate(ArrayList<Booking> bookings){
 		//this arraylist will hold all the months of the bookings
 		// e.g 10,9,2,2,3,12
@@ -53,11 +61,58 @@ public class BookingList {
 
 		//bookingIndex[i]'s month should be equal to sortedByMonth[i] so that you can then sort by day
 		ArrayList <Booking> bookingIndex = new ArrayList<Booking>();
-		
+
+		// add everything to the lists
 		for (int i = 0; i< bookings.size();i++)	{
 			bookingIndex.add(bookings.get(i));
 			sortedByMonth.add(bookings.get(i).getMonthFromDate());
 		}
+
+		//sort
+
+		for(int i = 0; i<sortedByMonth.toArray().length;i++){
+			for (int j = 0; j <sortedByMonth.toArray().length;j++){
+
+				if (sortedByMonth.get(i) > sortedByMonth.get(j)){
+					int temp = sortedByMonth.get(i);
+					//set arr[i] to [arr[j]
+					sortedByMonth.set(i,sortedByMonth.get(j));
+					sortedByMonth.set(j,temp);
+
+
+					//do same thing to bookingIndex
+					Booking tempBooking = bookingIndex.get(i);
+					bookingIndex.set(i,bookingIndex.get(j));
+					bookingIndex.set(j,tempBooking);
+
+
+				}
+
+			}
+		}
+
+
+
+
+
+		/*
+		for (int i = 0; i < n - 1; i++) {
+			swapped = false;
+			for (j = 0; j < n - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+
+					// Swap arr[j] and arr[j+1]
+					temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					swapped = true;
+				}
+			}
+		}
+
+		 */
+
+
 		
 		
 		
@@ -68,5 +123,6 @@ public class BookingList {
 	
 	//TODO make function that reads the data and adds the bookings into the array
 	//or just do it in database
-	
+
+
 }
