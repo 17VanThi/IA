bddd1025ef47b30f862d19f689dd109607e643a4
17thi_v <17thi_v@bexleygs.co.uk>
2023-06-08 11:46:10 +0100

    its broken!!!1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-       
+
+
+
         BookingList newBookingList = new BookingList();
         newBookingList.getBookingsFromData("data",newBookingList.getArrayList());
-        newBookingList.displayList();
-        newBookingList.displayList();
-        newBookingList.sortByDate();
-        newBookingList.displayList();
+
+
 
     }
 }
