a18c0d812b973ccac2b5cbac39b55989ba9d3d29
17thi_v <17thi_v@bexleygs.co.uk>
2023-07-04 09:26:58 +0100

    calender

--- src/com/company/Calender.java
+++ src/com/company/Calender.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Calender extends JFrame implements ActionListener {
     int month;
@@ -15,22 +16,31 @@ public class Calender extends JFrame implements ActionListener {
         int w = 50;
         int h = 50;
 
+        ArrayList<JButton> buttonList = new ArrayList<JButton>();
+
         int num = 1; // button label
+
 
         this.setPreferredSize(new Dimension(370,350)); //size of window
 
-        for(int i = 0; i < 5; i++ ){ //column
+        for(int i = 0; i < 4; i++ ){ //column
             x = 0 ;
+
             for(int j = 0; j < 7; j++){ //row
                 JButton button = new JButton(Integer.toString(num));
                 button.setBounds(x,y,w,h);
                 button.addActionListener(this);
                 getContentPane().add(button);
+                button.getBounds();
                 x = x+w;
                 num++;
+                buttonList.add(button);
             }
             y = y + 50;
         }
+        System.out.println(buttonList.get(buttonList.size()-1).getText());
+        buttonList.get(buttonList.size() - 1).setBounds(0,0,50,50);
+
         pack();
         setVisible(true);
     }
