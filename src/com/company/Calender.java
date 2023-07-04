c92e5843c60751ae77de2d5b99774bfc6f42a1a3
17thi_v <17thi_v@bexleygs.co.uk>
2023-07-04 09:00:43 +0100

    calender

--- src/com/company/Calender.java
+++ src/com/company/Calender.java
@@ -9,26 +9,35 @@ public class Calender extends JFrame implements ActionListener {
     int month;
     int year;
     public Calender(){
+
         int x = 0;
         int y = 0;
         int w = 50;
         int h = 50;
 
-        for(int i = 0; i < 7; i++ ){
-            for(int j = 0; j < 6; j++){
-                JButton button = new JButton(Integer.toString(i*j));
+        int num = 1; // button label
+
+        this.setPreferredSize(new Dimension(370,350)); //size of window
+
+        for(int i = 0; i < 5; i++ ){ //column
+            x = 0 ;
+            for(int j = 0; j < 7; j++){ //row
+                JButton button = new JButton(Integer.toString(num));
                 button.setBounds(x,y,w,h);
                 button.addActionListener(this);
                 getContentPane().add(button);
                 x = x+w;
+                num++;
             }
+            y = y + 50;
         }
-
         pack();
         setVisible(true);
-
     }
+
+
     @Override
+    //when any button is clicked
     public void actionPerformed(ActionEvent e) {
 
     }
